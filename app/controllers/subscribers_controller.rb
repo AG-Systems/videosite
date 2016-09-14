7925b2bb5408959ddf0f238c73b716cb4f44da6d
Auriga <area511alienware@gmail.com>
2016-09-14 03:43:12 +0000

    Trying to get the payments work

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -2,6 +2,19 @@ class SubscribersController < ApplicationController
     before_filter :authenticate_user!
     
     def new
+        token = params[:stripeToken]
+        
+        customer = Stripe::Customer.create(
+            card: token,
+            plan: 1020,
+            email: current_sign_in.email
+        
+            )
+            current_user.subscribed = true
+            current_user.stripeid = customer.id
+            current_user.save
+            
+            redirect_to root_path
     end
     def create
     end
