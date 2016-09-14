974e91aa1cc10029dfb6aa7722b409b1da74a730
Auriga <area511alienware@gmail.com>
2016-09-14 04:10:13 +0000

    Trying to get the payments work

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -19,17 +19,17 @@ class SubscribersController < ApplicationController
     end
     def update
     
-        token = params[:stripeToken]
+        #token = params[:stripeToken]
         
-        customer = Stripe::Customer.create(
-            card: token,
-            plan: 1020,
-            email: current_sign_in.email
+        #customer = Stripe::Customer.create(
+           # card: token,
+           # plan: 1020,
+          #  email: current_sign_in.email
         
-            )
-            current_user.subscribed = true
-            current_user.stripeid = customer.id
-            current_user.save
-            redirect_to root_path
+           # )
+          #  current_user.subscribed = true
+          #  current_user.stripeid = customer.id
+          #  current_user.save
+          #  redirect_to root_path
     end
 end
